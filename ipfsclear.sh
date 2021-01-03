18664dbcf14623ce6f3ce4dcde4dba896ff74c42
Pratama Pangestu <p7atama@protonmail.ch>
2021-01-04 03:12:42 +0700

    instant clear ipfs cache on 4 node in idena-manager

---
+++ ipfsclear.sh
@@ -0,0 +1,20 @@
+idena-manager disable
+rm log*
+cd datadir-node1
+rm -r ipfs
+rm -r logs
+cd ..
+cd datadir-node2
+rm -r ipfs
+rm -r logs
+cd ..
+cd datadir-node3
+rm -r ipfs
+rm -r logs
+cd ..
+cd datadir-node4
+rm -r ipfs
+rm -r logs
+cd ..
+idena-manager enable
+idena-manager status
