2dc781ddb18eec4ff5205692f2b01686a1c798f0
Pratama Pangestu <p7atama@protonmail.ch>
2021-01-03 13:06:57 +0700

    add script

---
+++ 4node.sh
@@ -0,0 +1,48 @@
+source <(curl -sL https://bit.ly/idena-manager-installer)
+idena-manager add
+idena-manager disable
+cd datadir-node1
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+sudo apt-get install unzip
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip
+rm idenachain.db.zip
+cd ..
+cd ..
+idena-manager add
+idena-manager disable
+cd datadir-node2
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip
+rm idenachain.db.zip
+cd ..
+cd ..
+idena-manager add
+idena-manager disable
+cd datadir-node3
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip
+rm idenachain.db.zip
+cd ..
+cd ..
+idena-manager add
+idena-manager disable
+cd datadir-node4
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip
+rm idenachain.db.zip
+cd ..
+cd ..
+idena-manager enable
+idena-manager status
