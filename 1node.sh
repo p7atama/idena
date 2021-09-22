1cc948ffe724a8719a62f9348e34a455bb2c5323
Pratama Pangestu <pratamapangestu77@protonmail.com>
2021-09-22 20:51:51 +0700

    Create 1node.sh

---
+++ 1node.sh
@@ -0,0 +1,15 @@
+source <(curl -sL https://bit.ly/idena-manager-installer)
+sudo fallocate -l 1G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
+idena-manager add
+idena-manager disable
+cd datadir-node1
+rm -r idenachain.db
+rm -r ipfs
+mkdir idenachain.db
+cd idenachain.db
+sudo apt-get install unzip
+wget "https://sync.idena.site/idenachain.db.zip"
+unzip idenachain.db.zip
+rm idenachain.db.zip
+cd ..
+cd ..
