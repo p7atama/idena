16e1e2d6c2072f8e73f4171afd0eca53d2465be8
Pratama Pangestu <pratamapangestu77@protonmail.com>
2022-09-01 00:54:17 +0700

    Update 1node.sh

--- 1node.sh
+++ 1node.sh
@@ -8,7 +8,7 @@ rm -r ipfs
 mkdir idenachain.db
 cd idenachain.db
 sudo apt-get install unzip
-wget "https://pool.idena.site/idenachain.db.zip"
+wget "https://sync.idena.site/idenachain.db.zip"
 unzip idenachain.db.zip
 rm idenachain.db.zip
 cd ..
